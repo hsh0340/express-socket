c128d572c9929b2d22dfbc25ee8785bd8d65241f
hsh0340 <hsh0340@naver.com>
2023-09-12 21:59:06 +0900

    publishing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 const PORT = 4000;
 app.listen(PORT, () => {
